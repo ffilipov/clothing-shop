2c3a1c133eaaf57d8342b7dbc36abc007a91de80
Filip Filipov <filip.d.filipov@stanga.net>
2024-05-10 18:15:45 +0300

    logout added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,32 @@
 import { Outlet, Link } from "react-router-dom";
-import { Fragment } from "react";
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+import { Fragment, useState, useEffect } from "react";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 import "./navigation.styles.scss";
 
+import { signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../utils/firebase/firebase.utils';
+
 const Navigation = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const logoutUser = async () => {
+    try {
+      await signOut(auth);
+      setCurrentUser(null);
+      console.log('User logged out.');
+    } catch (error) {
+      console.log('Error logging out:', error);
+    }
+  };
+
   return (
     <Fragment>
       <div className="navigation">
@@ -11,8 +34,18 @@ const Navigation = () => {
           <Logo className="logo" />
         </Link>
         <div className="nav-links-container">
-            <Link className="nav-link" to="/shop">SHOP</Link>
-            <Link className="nav-link" to="/auth">SIGN IN</Link>
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          {currentUser ? (
+            <Link className="nav-link" to="/" onClick={logoutUser}>
+              SIGN OUT
+            </Link>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
